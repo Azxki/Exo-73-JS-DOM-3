103a94767320f288effb3b6e80375e990e836637
azxki <cococoursan@gmail.com>
2022-11-23 11:31:10 +0100

    EXO

--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,2 +1,13 @@
-let body = document.getElementsByTagName("body");
-let body = document.createElement("div");
+
+
+let div = document.createElement('div');
+let div_2 = document.createElement('div');
+let body = document.getElementById("body");
+
+div.className = "planet";
+div.style.backgroundColor = "blue";
+div_2.className = "moon";
+
+body.appendChild(div);
+div.appendChild(div_2);
+
